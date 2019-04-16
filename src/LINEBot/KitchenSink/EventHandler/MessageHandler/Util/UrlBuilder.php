01c6dc5b0f5b362cb7128c12342e81b2f2a8e92f
morimorim <mori.shunsuke@linecorp.com>
2019-04-16 16:39:02 +0900

    add

--- src/LINEBot/KitchenSink/EventHandler/MessageHandler/Util/UrlBuilder.php
+++ src/LINEBot/KitchenSink/EventHandler/MessageHandler/Util/UrlBuilder.php
@@ -24,7 +24,7 @@ class UrlBuilder
     {
         // NOTE: You should configure $baseUri according to your environment
         // Perhaps, it is prefer to use $_SERVER['HTTP_HOST'], $_SERVER['HTTP_X_FORWARDED_HOST'] or etc
-        $baseUri = $req->getUri()->getBaseUrl();
+        $baseUri = str_replace('http://', 'https://', $req->getUri()->getBaseUrl());
         foreach ($paths as $path) {
             $baseUri .= '/' . urlencode($path);
         }
